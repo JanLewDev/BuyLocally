21e87fc5dff4ba5129868a5d84b5be491fb5bae6
Jan <jasio.040404@gmail.com>
2022-05-04 17:05:34 +0100

    Second Prototype 1

--- app/src/main/java/com/example/firstprototype/ConfigEmail.java
+++
@@ -1,7 +0,0 @@
-package com.example.firstprototype;
-
-public class ConfigEmail {
-
-    public static final String EMAIL = "buylocally2022@gmail.com";
-    public static final String PASSWORD = "beeeeeee";
-}
